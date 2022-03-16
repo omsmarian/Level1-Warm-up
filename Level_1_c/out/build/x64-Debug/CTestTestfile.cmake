5faeb8872087935b3630677616e4e0ef0f0dc568
omsmarian <80849429+omsmarian@users.noreply.github.com>
2022-03-16 20:13:48 -0300

    Delete Level_1_c directory

--- Level_1_c/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dell/Pictures/Itba/EDA/Level 1
-# Build directory: D:/Dell/Pictures/Itba/EDA/Level 1/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "D:/Dell/Pictures/Itba/EDA/Level 1/out/build/x64-Debug/orbitalsim_test.exe")
-set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "D:/Dell/Pictures/Itba/EDA/Level 1/CMakeLists.txt;26;add_test;D:/Dell/Pictures/Itba/EDA/Level 1/CMakeLists.txt;0;")
